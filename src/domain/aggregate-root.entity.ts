b39982edb6af851242447ace3858b42bc3992e7b
Arslan <rslnkrmt2552@gmail.com>
2022-09-07 00:51:58 +0500

    add aggregate root (logical separation from base entity)

---
+++ src/domain/aggregate-root.entity.ts
@@ -0,0 +1,3 @@
+import { BaseEntity } from './base.entity';
+
+export abstract class AggregateRoot extends BaseEntity {}
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,2 +1,3 @@
 export * from './base.entity';
 export * from './types';
+export * from './aggregate-root.entity';
