d4a9000543bd31380310f105901a613e2106ffa2
Arslan <rslnkrmt2552@gmail.com>
2022-11-10 00:36:22 +0500

    random formatting

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,30 +6,30 @@ import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import externals from 'rollup-plugin-node-externals';
 
 const config: RollupOptions =
-  // CommonJS (for Node) & ESM (for bundlers)
-  {
-    input: 'src/index.ts',
-    output: [
-      { file: pkg.main, format: 'cjs', sourcemap: false },
-      { file: pkg.module, format: 'es', sourcemap: false },
-    ],
-    plugins: [
-      typescriptPaths({ preserveExtensions: true }),
-      externals(),
-      swc(
-        defineRollupSwcOption({
-          minify: true,
-          tsconfig: './tsconfig.build.json',
-          jsc: {
-            minify: {
-              mangle: false,
-              compress: true,
-            },
-          },
-        }),
-      ),
-    ],
-    // external: ['oxide.ts', 'oxide.ts/dist/core', 'node:crypto'],
-  };
+	// CommonJS (for Node) & ESM (for bundlers)
+	{
+		input: 'src/index.ts',
+		output: [
+			{ file: pkg.main, format: 'cjs', sourcemap: false },
+			{ file: pkg.module, format: 'es', sourcemap: false },
+		],
+		plugins: [
+			typescriptPaths({ preserveExtensions: true }),
+			externals(),
+			swc(
+				defineRollupSwcOption({
+					minify: true,
+					tsconfig: './tsconfig.build.json',
+					jsc: {
+						minify: {
+							mangle: false,
+							compress: true,
+						},
+					},
+				}),
+			),
+		],
+		// external: ['oxide.ts', 'oxide.ts/dist/core', 'node:crypto'],
+	};
 
 export default config;
