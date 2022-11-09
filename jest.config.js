a84958bc45a7e7dbb9d01bda13085e0230a89c5f
Arslan <rslnkrmt2552@gmail.com>
2022-11-10 00:45:04 +0500

    finalize rome config

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,22 @@
 
 /** @type {require('@jest/types').Config.InitialOptions} */
 const config = {
-  moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'node',
-  notify: false,
-  rootDir: '.',
-  roots: ['src', 'tests'],
-  testRegex: '.*\\.(spec|test|integration)\\.ts',
-  testPathIgnorePatterns: ['/node_modules/'],
-  transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
-  },
-  transformIgnorePatterns: ['/node_modules/'],
-  moduleNameMapper: {
-    '^@carbonteq/hexapp(.*)$': ['<rootDir>/lib'],
-    '^oxide.ts/(.*)$': ['<rootDir>/node_modules/oxide.ts/$1'],
-  },
-  setupFilesAfterEnv: ['jest-extended/all'],
+	moduleFileExtensions: ['js', 'ts'],
+	testEnvironment: 'node',
+	notify: false,
+	rootDir: '.',
+	roots: ['src', 'tests'],
+	testRegex: '.*\\.(spec|test|integration)\\.ts',
+	testPathIgnorePatterns: ['/node_modules/'],
+	transform: {
+		'^.+\\.(t|j)sx?$': '@swc/jest',
+	},
+	transformIgnorePatterns: ['/node_modules/'],
+	moduleNameMapper: {
+		'^@carbonteq/hexapp(.*)$': ['<rootDir>/lib'],
+		'^oxide.ts/(.*)$': ['<rootDir>/node_modules/oxide.ts/$1'],
+	},
+	setupFilesAfterEnv: ['jest-extended/all'],
 };
 
 module.exports = config;
