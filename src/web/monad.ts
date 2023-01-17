4d371fb9723d774a3606326b9e66d22e40cc20d5
Arslan <rslnkrmt2552@gmail.com>
2023-01-17 12:01:19 +0500

    fix lint errors

--- src/web/monad.ts
+++ src/web/monad.ts
@@ -6,58 +6,58 @@ const errPred = <T, E>(el: Result<T, E>): boolean => el.isErr();
 const preds = [okPred, errPred];
 
 export class Monadic {
-  static bind<T, E, U>(r: Result<T, E>, f: (val: T) => Result<U, E>) {
-    if (r.isErr()) return r;
+	static bind<T, E, U>(r: Result<T, E>, f: (val: T) => Result<U, E>) {
+		if (r.isErr()) return r;
 
-    const val = r.unwrap();
+		const val = r.unwrap();
 
-    return f(val);
-  }
+		return f(val);
+	}
 
-  static async bindAsync<T, E, U>(
-    r: Result<T, E>,
-    f: (val: T) => Promise<Result<U, E>>,
-  ) {
-    if (r.isErr()) return r;
+	static async bindAsync<T, E, U>(
+		r: Result<T, E>,
+		f: (val: T) => Promise<Result<U, E>>,
+	) {
+		if (r.isErr()) return r;
 
-    const val = r.unwrap();
+		const val = r.unwrap();
 
-    return await f(val);
-  }
+		return await f(val);
+	}
 
-  static sequence<T, E>(r: Array<Result<T, E>>): Result<Array<T>, E> {
-    const results = [];
+	static sequence<T, E>(r: Array<Result<T, E>>): Result<Array<T>, E> {
+		const results = [];
 
-    for (const el of r) {
-      if (el.isErr()) return el;
+		for (const el of r) {
+			if (el.isErr()) return el;
 
-      results.push(el.unwrap());
-    }
+			results.push(el.unwrap());
+		}
 
-    return Ok(results);
-  }
+		return Ok(results);
+	}
 
-  static allM<T, E>(c: OkOrErr, seq: Result<T, E>[]): boolean {
-    let pred = preds[Number(c == 'Ok')];
+	static allM<T, E>(c: OkOrErr, seq: Result<T, E>[]): boolean {
+		const pred = preds[Number(c === 'Ok')];
 
-    for (const el of seq) {
-      if (!pred(el)) {
-        return false;
-      }
-    }
+		for (const el of seq) {
+			if (!pred(el)) {
+				return false;
+			}
+		}
 
-    return true;
-  }
+		return true;
+	}
 
-  static anyM<T, E>(c: OkOrErr, seq: Result<T, E>[]): boolean {
-    let pred = preds[Number(c == 'Ok')];
+	static anyM<T, E>(c: OkOrErr, seq: Result<T, E>[]): boolean {
+		const pred = preds[Number(c === 'Ok')];
 
-    for (const el of seq) {
-      if (pred(el)) {
-        return true;
-      }
-    }
+		for (const el of seq) {
+			if (pred(el)) {
+				return true;
+			}
+		}
 
-    return false;
-  }
+		return false;
+	}
 }
