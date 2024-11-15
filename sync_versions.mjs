032912b3cfccca9bf277621fb19c1077d0462b6c
Arslan <rslnkrmt2552@gmail.com>
2024-11-15 16:33:04 +0100

    ready for jsr?

---
+++ sync_versions.mjs
@@ -0,0 +1,16 @@
+import { readFileSync, writeFileSync } from "node:fs";
+
+const pkgBuff = readFileSync("./package.json");
+const pkg = JSON.parse(pkgBuff);
+
+const jsrBuff = readFileSync("./deno.jsonc");
+const jsrCurr = JSON.parse(jsrBuff);
+
+if (jsrCurr.version !== pkg.version) {
+	console.log("Updating deno version");
+	jsrCurr.version = pkg.version;
+
+	writeFileSync("./deno.jsonc", JSON.stringify(jsrCurr));
+} else {
+	console.log("deno version already up to date");
+}
